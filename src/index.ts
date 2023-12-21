ac5d870c7a643784c615a1d131ff8a092166b29c
daniel-lujan <daniel.lujan@udea.edu.co>
2023-12-21 15:22:57 -0500

    feat: :sparkles: Add CORS and JSON middlewares

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import dotenv from 'dotenv';
 import connectDB from './database/connection';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -9,6 +10,15 @@ connectDB();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CORS_ORIGIN ?? '',
+  })
+);
+
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`API listening at PORT ${port}`);
